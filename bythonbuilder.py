64853e09ad7bfab977af96ae66c579c8276a28b1
klaus-76996 <3764519014@qq.com>
2025-01-17 18:30:39 +0800

    Add files via upload

---
+++ bythonbuilder.py
@@ -0,0 +1,270 @@
+a="""[black on green] SUCCESS [/black on green][green]      正在加载组件   2/113 [Built-In] 返回主城@0.0.1
+[black on green] SUCCESS [/black on green][green]      正在加载组件   3/113 [Core] 改名记录@0.0.1
+[black on yellow] WARNING [/black on yellow][yellow]      跳过加载组件   4/113 [Built-In] 设置重生点@0.0.1
+[black on yellow] WARNING [/black on yellow][yellow]      跳过加载组件   5/113 [Built-In] 玩家自杀@0.0.1
+[black on yellow] WARNING [/black on yellow][yellow]      跳过加载组件   6/113 [Built-In] 入服欢迎@0.0.1
+[black on yellow] WARNING [/black on yellow][yellow]      跳过加载组件   7/113 [Built-In] 排行榜@0.0.2
+[black on yellow] WARNING [/black on yellow][yellow]      跳过加载组件   8/113 [Built-In] 排行榜@0.0.2
+[black on yellow] WARNING [/black on yellow][yellow]      跳过加载组件   9/113 [Built-In] 计划任务@0.0.1
+[black on yellow] WARNING [/black on yellow][yellow]      跳过加载组件  10/113 [Built-In] 入服欢迎@0.0.1
+[black on yellow] WARNING [/black on yellow][yellow]      跳过加载组件  11/113 [Built-In] 时间同步@0.0.1
+[black on yellow] WARNING [/black on yellow][yellow]      跳过加载组件  12/113 [Built-In] 计划任务@0.0.1
+[black on yellow] WARNING [/black on yellow][yellow]      跳过加载组件  13/113 [Built-In] 排行榜@0.0.2
+[black on yellow] WARNING [/black on yellow][yellow]      跳过加载组件  14/113 [Built-In] 皮肤检查@0.0.2
+[black on yellow] WARNING [/black on yellow][yellow]      跳过加载组件  15/113 [Built-In] 排行榜@0.0.2
+[black on yellow] WARNING [/black on yellow][yellow]      跳过加载组件  16/113 [Built-In] 每日签到@0.0.1
+[black on yellow] WARNING [/black on yellow][yellow]      跳过加载组件  17/113 [Built-In] 系统上线提示@0.0.1
+[black on yellow] WARNING [/black on yellow][yellow]      跳过加载组件  18/113 [Built-In] 计划任务@0.0.1
+[black on yellow] WARNING [/black on yellow][yellow]      跳过加载组件  19/113 [Built-In] 排行榜@0.0.2
+[black on yellow] WARNING [/black on yellow][yellow]      跳过加载组件  20/113 [Built-In] 返回死亡点@0.0.1
+[black on green] SUCCESS [/black on green][green]      正在加载组件  21/113 [Built-In] 入服欢迎@0.0.1
+[black on yellow] WARNING [/black on yellow][yellow]      跳过加载组件  22/113 [Built-In] 第三方_by温柔_地皮组件@0.0.1
+[black on yellow] WARNING [/black on yellow][yellow]      跳过加载组件  23/113 [Built-In] 第三方_by温柔_优化版本雪球菜单组件@0.0.1
+[black on green] SUCCESS [/black on green][green]      正在加载组件  24/113 [Core] OP权限自检@0.0.1
+[black on green] SUCCESS [/black on green][green]      正在加载组件  25/113 [Built-In] 区域扫描@0.0.1
+[black on green] SUCCESS [/black on green][green]      正在加载组件  26/113 [Built-In] 同步退出@0.0.1
+[black on green] SUCCESS [/black on green][green]      正在加载组件  27/113 [Built-In] 统一导入系统@0.0.4
+[black on green] SUCCESS [/black on green][green]      正在加载组件  28/113 [Built-In] 统一导出系统@0.0.2
+[black on green] SUCCESS [/black on green][green]      正在加载组件  29/113 [Core] 性能分析@0.0.1
+[black on green] SUCCESS [/black on green][green]      正在加载组件  30/113 [Core] 指令发送@0.0.2
+[black on yellow] WARNING [/black on yellow][yellow]      跳过加载组件  31/113 [Built-In] 数据包捕获@0.0.2
+[black on yellow] WARNING [/black on yellow][yellow]      跳过加载组件  32/113 [Built-In] 切换@0.0.2
+[black on yellow] WARNING [/black on yellow][yellow]      跳过加载组件  33/113 [Built-In] 简单自定义指令@0.0.2
+[black on yellow] WARNING [/black on yellow][yellow]      跳过加载组件  34/113 [Built-In] 简单自定义指令@0.0.2
+[black on yellow] WARNING [/black on yellow][yellow]      跳过加载组件  35/113 [Built-In] 反馈信息@0.0.1
+[black on yellow] WARNING [/black on yellow][yellow]      跳过加载组件  36/113 [Built-In] 简单自定义指令@0.0.2
+[black on yellow] WARNING [/black on yellow][yellow]      跳过加载组件  37/113 [Built-In] 简单自定义指令@0.0.2
+[black on yellow] WARNING [/black on yellow][yellow]      跳过加载组件  38/113 [Built-In] 简单自定义指令@0.0.2
+[black on yellow] WARNING [/black on yellow][yellow]      跳过加载组件  39/113 [Built-In] 简单自定义指令@0.0.2
+[black on yellow] WARNING [/black on yellow][yellow]      跳过加载组件  40/113 [Built-In] 简单自定义指令@0.0.2
+[black on yellow] WARNING [/black on yellow][yellow]      跳过加载组件  41/113 [Built-In] 简单自定义指令@0.0.2
+[black on yellow] WARNING [/black on yellow][yellow]      跳过加载组件  42/113 [Built-In] 切换@0.0.2
+[black on yellow] WARNING [/black on yellow][yellow]      跳过加载组件  43/113 [Built-In] 切换@0.0.2
+[black on yellow] WARNING [/black on yellow][yellow]      跳过加载组件  44/113 [Built-In] 玩家信息@0.0.1
+[black on yellow] WARNING [/black on yellow][yellow]      跳过加载组件  45/113 [Built-In] 切换@0.0.2
+[black on yellow] WARNING [/black on yellow][yellow]      跳过加载组件  46/113 [Built-In] 切换@0.0.2
+[black on yellow] WARNING [/black on yellow][yellow]      跳过加载组件  47/113 [Built-In] 玩家留言@0.0.1
+[black on yellow] WARNING [/black on yellow][yellow]      跳过加载组件  48/113 [Built-In] 切换@0.0.2
+[black on yellow] WARNING [/black on yellow][yellow]      跳过加载组件  49/113 [Built-In] 计分板UID追踪@0.0.1
+[black on yellow] WARNING [/black on yellow][yellow]      跳过加载组件  50/113 [Built-In] 切换@0.0.2
+[black on yellow] WARNING [/black on yellow][yellow]      跳过加载组件  51/113 [Built-In] 切换@0.0.2
+[black on yellow] WARNING [/black on yellow][yellow]      跳过加载组件  52/113 [Built-In] 切换@0.0.2
+[black on yellow] WARNING [/black on yellow][yellow]      跳过加载组件  53/113 [Built-In] 切换@0.0.2
+[black on yellow] WARNING [/black on yellow][yellow]      跳过加载组件  54/113 [Built-In] 切换@0.0.2
+[black on yellow] WARNING [/black on yellow][yellow]      跳过加载组件  55/113 [Built-In] 切换@0.0.2
+[black on green] SUCCESS [/black on green][green]      正在加载组件  56/113 [Built-In] 存档修复@0.0.1
+[black on yellow] WARNING [/black on yellow][yellow]      跳过加载组件  57/113 [Built-In] 小木斧@0.0.1
+[black on yellow] WARNING [/black on yellow][yellow]      跳过加载组件  58/113 [Built-In] OP权限模拟@0.0.01
+[black on yellow] WARNING [/black on yellow][yellow]      跳过加载组件  59/113 [Built-In] 自助建筑备份@0.0.1
+[black on yellow] WARNING [/black on yellow][yellow]      跳过加载组件  60/113 [Built-In] 手持32k检测@0.0.1
+[black on yellow] WARNING [/black on yellow][yellow]      跳过加载组件  61/113 [Built-In] 32k方块检测@0.0.1
+[black on yellow] WARNING [/black on yellow][yellow]      跳过加载组件  62/113 [Built-In] 刷怪笼检测@0.0.1
+[black on yellow] WARNING [/black on yellow][yellow]      跳过加载组件  63/113 [Built-In] 发言频率限制@0.0.1
+[black on yellow] WARNING [/black on yellow][yellow]      跳过加载组件  64/113 [Built-In] 封禁时间@0.0.1
+[black on yellow] WARNING [/black on yellow][yellow]      跳过加载组件  65/113 [Built-In] 踢人@0.0.1
+[black on yellow] WARNING [/black on yellow][yellow]      跳过加载组件  66/113 [Built-In] 踢人@0.0.1
+[black on yellow] WARNING [/black on yellow][yellow]      跳过加载组件  67/113 [Built-In] 踢人@0.0.1
+[black on yellow] WARNING [/black on yellow][yellow]      跳过加载组件  68/113 [Built-In] 管理员检测@0.0.1
+[black on yellow] WARNING [/black on yellow][yellow]      跳过加载组件  69/113 [Built-In] 消除方块@0.0.1
+[black on yellow] WARNING [/black on yellow][yellow]      跳过加载组件  70/113 [Built-In] 违规昵称检测@0.0.1
+[black on yellow] WARNING [/black on yellow][yellow]      跳过加载组件  71/113 [Built-In] 计划任务@0.0.1
+[black on yellow] WARNING [/black on yellow][yellow]      跳过加载组件  72/113 [Built-In] 高频红石检查@0.0.1
+[black on yellow] WARNING [/black on yellow][yellow]      跳过加载组件  73/113 [Built-In] 兑换码@0.0.1
+[black on yellow] WARNING [/black on yellow][yellow]      跳过加载组件  74/113 [Built-In] 物品回收@0.0.1
+[black on yellow] WARNING [/black on yellow][yellow]      跳过加载组件  75/113 [Built-In] 玩家商店@0.0.1
+[black on yellow] WARNING [/black on yellow][yellow]      跳过加载组件  76/113 [Built-In] 玩家转账@0.0.1
+[black on yellow] WARNING [/black on yellow][yellow]      跳过加载组件  77/113 [Built-In] 商店@0.0.1
+[black on yellow] WARNING [/black on yellow][yellow]      跳过加载组件  78/113 [Built-In] 快递系统@0.0.1
+[black on green] SUCCESS [/black on green][green]      正在加载组件  79/113 [Built-In] 玩家互传@0.0.1
+[black on yellow] WARNING [/black on yellow][yellow]      跳过加载组件  80/113 [Built-In] 返回死亡点@0.0.1
+[black on green] SUCCESS [/black on green][green]      正在加载组件  81/113 [Built-In] 自定义传送点@0.0.2
+[black on yellow] WARNING [/black on yellow][yellow]      跳过加载组件  82/113 [Built-In] 小说阅读器@0.0.1
+[black on green] SUCCESS [/black on green][green]      正在加载组件  83/113 [Core] 数据库导入导出工具@0.0.1
+[black on green] SUCCESS [/black on green][green]      正在加载组件  84/113 [Built-In] 聊天记录@0.0.1
+[black on green] SUCCESS [/black on green][green]      正在加载组件  85/113 [Built-In] OmegaSide旁加载组件系统@0.0.3
+[black on green] SUCCESS [/black on green][green]      正在加载组件  86/113 [Core] 菜单显示@0.0.1
+[black on yellow] WARNING [/black on yellow][yellow]      跳过加载组件  87/113 [Built-In] 服服互通@0.0.3
+[black on yellow] WARNING [/black on yellow][yellow]      跳过加载组件  88/113 [Built-In] 群服互通@0.0.2
+[black on yellow] WARNING [/black on yellow][yellow]      跳过加载组件  89/113 [Built-In] 简单自定义指令@0.0.2
+[black on yellow] WARNING [/black on yellow][yellow]      跳过加载组件  90/113 [Built-In] 排行榜@0.0.2
+[black on yellow] WARNING [/black on yellow][yellow]      跳过加载组件  91/113 [Built-In] 踢人@0.0.1
+[black on yellow] WARNING [/black on yellow][yellow]      跳过加载组件  92/113 [Built-In] 玩家留言@0.0.1
+[black on yellow] WARNING [/black on yellow][yellow]      跳过加载组件  93/113 [Built-In] 简单自定义指令@0.0.2
+[black on yellow] WARNING [/black on yellow][yellow]      跳过加载组件  94/113 [Built-In] 简单自定义指令@0.0.2
+[black on yellow] WARNING [/black on yellow][yellow]      跳过加载组件  95/113 [Built-In] 简单自定义指令@0.0.2
+[black on yellow] WARNING [/black on yellow][yellow]      跳过加载组件  96/113 [Built-In] 简单自定义指令@0.0.2
+[black on yellow] WARNING [/black on yellow][yellow]      跳过加载组件  97/113 [Built-In] 计划任务@0.0.1
+[black on yellow] WARNING [/black on yellow][yellow]      跳过加载组件  98/113 [Built-In] 简单自定义指令@0.0.1
+[black on green] SUCCESS [/black on green][green]      正在加载组件  99/113 [Core] 随机巡逻@0.0.2
+[black on yellow] WARNING [/black on yellow][yellow]      跳过加载组件 100/113 [第三方] 2401PT::喵喵喵@0.0.1
+[black on yellow] WARNING [/black on yellow][yellow]      跳过加载组件 101/113 [第三方] Liliya233::能力管理@0.0.1
+[black on yellow] WARNING [/black on yellow][yellow]      跳过加载组件 102/113 [第三方] Liliya233::选取方块@0.0.1
+[black on yellow] WARNING [/black on yellow][yellow]      跳过加载组件 103/113 [第三方] Happy2018new::记录方块改动日志@0.0.1
+[black on yellow] WARNING [/black on yellow][yellow]      跳过加载组件 104/113 [第三方] Liliya233::入睡检测@0.0.1
+[black on yellow] WARNING [/black on yellow][yellow]      跳过加载组件 105/113 [第三方] Liliya233::挂机检测@0.0.1
+[black on yellow] WARNING [/black on yellow][yellow]      跳过加载组件 106/113 [第三方] Liliya233::频服互通@0.0.1
+[black on yellow] WARNING [/black on yellow][yellow]      跳过加载组件 107/113 [第三方] 温柔::Pvp系统@0.0.2
+[black on yellow] WARNING [/black on yellow][yellow]      跳过加载组件 108/113 [第三方] 温柔::抽奖@0.0.6
+[black on yellow] WARNING [/black on yellow][yellow]      跳过加载组件 109/113 [第三方] 温柔::无前缀简单自定义指令@0.0.1
+[black on yellow] WARNING [/black on yellow][yellow]      跳过加载组件 110/113 [第三方] 温柔::自定义附魔升级系统@0.0.1
+[black on yellow] WARNING [/black on yellow][yellow]      跳过加载组件 111/113 [第三方] 温柔::虚拟称号系统@0.0.2
+[black on yellow] WARNING [/black on yellow][yellow]      跳过加载组件 112/113 [第三方] 温柔::虚拟装备系统@0.0.2
+[black on yellow] WARNING [/black on yellow][yellow]      跳过加载组件 113/113 [第三方] 温柔::银行系统@0.0.2"""
+b="""32k方块检测
+OmegaSide旁加载组件系统
+OP权限模拟
+OP权限自检
+菜单显示
+存档修复
+第三方_by温柔_地皮组件
+第三方_by温柔_优化版本雪球菜单组件
+兑换码
+发言频率限制
+反馈信息
+返回死亡点
+返回主城
+封禁时间
+服服互通
+改名记录
+高频红石检查
+管理员检测
+计分板UID追踪
+计划任务
+假死检测
+简单自定义指令
+快递系统
+聊天记录
+每日签到
+排行榜
+皮肤检查
+切换
+区域扫描
+群服互通
+入服欢迎
+商店
+设置重生点
+时间同步
+手持32k检测
+数据包捕获
+数据库导入导出工具
+刷怪笼检测
+踢人
+同步退出
+统一导出系统
+统一导入系统
+玩家互传
+玩家留言
+玩家商店
+玩家信息
+玩家转账
+玩家自杀
+违规昵称检测
+物品回收
+系统上线提示
+消除方块
+小木斧
+小说阅读器
+性能分析
+指令发送
+自定义传送点
+自助建筑备份
+主系统"""
+if 1:
+
+    import time,os,json,random,ast
+    from rich import print as rprint
+    if not os.path.isdir("配置"):
+        os.makedirs("配置")
+    for i in b.split():
+        if not os.path.isfile(os.path.join("配置",f"{i}.json")):
+            with open(os.path.join("配置",f"{i}.json"),"w", encoding = "utf-8") as f:
+                num=random.randint(0,1)
+                f.write(json.dumps({"名称":i,"是否禁用":bool(num),"配置":{}},ensure_ascii=False, indent=2))
+    files=[]
+    for file in os.listdir(os.path.join("配置")):
+        files.append(file)
+    filesnum=0
+    
+    while True:
+        rprint("""DEBUG: Try to use high precision timer
+┌────────────────────────────────────────────────┐
+| [#0eeae3]https://gitee.com/mono163/PhoenixBuilder       [/#0eeae3]|
+└────────────────────────────────────────────────┘
+[yellow]bythonBuilder 6.6.7 (1a2b3c)[/yellow]
+Warning: Feature readline is not compatible with current platform.
+已启用语言：简体中文""")
+        text="正在检查更新，请稍等…"
+        spacer = '\r{{:{}}}'.format(len(text)) # '\r{:30}' for this s
+        spacer2 = '\r{{:{}}}'.format(len(text+"成功"))
+        print(spacer.format(text), end='')
+        time.sleep(2)
+        print(spacer2.format(text+"成功"), end='')
+        print()
+        time.sleep(1)
+        while True:
+            sever=input("请输入租赁服号:")
+            if sever =="":
+                rprint("[red] ERROR 租赁服号不能为空")
+            else:
+                try:
+                    int(sever)
+                    break
+                except:
+                    rprint("[red] ERROR 租赁服号错误")
+                
+        password=input("请输入租赁服密码[没有请留空]:")
+        time.sleep(1)
+        rprint("[yellow]服务器号: %s[/yellow]"%sever)
+        time.sleep(1)
+        rprint("[yellow]成功连接至服务器[/yellow]")
+        rprint("[yellow]请使用脑电波通信,地址:0.0.0.0[/yellow]")
+        rprint("[black on green] SUCCESS [/black on green][green] 如果Omega/bythonbuilder 崩溃了,它将会在一段时间后自动重启")
+        time.sleep(1)
+        for i in files:
+            time.sleep(0.005)
+            filesnum+=1
+            with open(os.path.join("配置",f"{i}"),"r", encoding = "utf-8") as f:
+                data=json.loads(f.read())
+            if 1:
+                if data["是否禁用"] == True:
+                    rprint(f"[black on yellow] WARNING [/black on yellow][yellow]      跳过加载组件 {filesnum}/{len(files)} [Built-In] {i[:-5]}@0.0.1")
+                else:
+                    rprint(f"[black on green] SUCCESS [/black on green][green]      正在加载组件 {filesnum}/{len(files)} [Built-In] {i[:-5]}@0.0.1")
+        #for c in a.split("\n"):
+        #    rprint(c)
+        #    time.sleep(0.005)
+        speed=10
+        rprint("[black on green] SUCCESS [/black on green][green] 成功加载bythonbuilder -OMEGA ,输入menu打开菜单")
+        while True:
+            
+            msg=input("")
+            if msg == "":
+                pass
+            elif msg[0:4] == "exit":
+                rprint("[black on yellow] WARNING [/black on yellow][yellow] 将在 5 秒后自动重启")
+                time.sleep(4)
+                break
+            elif msg[0:4] == "menu":
+                rprint("[yellow] 软件作者:Mono[/yellow][green]\n功能列表:\n1.load <文件名> 导入建筑\n2.speed <数字> 设置速度\n3.exit 退出/重启[/green]")
+            elif msg[0:5] == "speed":
+                speed=int(msg[5:])
+                rprint("[black on green] SUCCESS [/black on green][green] 设置速度为:"+msg[5:])
+            elif msg[0:4] == "set ":
+                rprint("[black on green] SUCCESS [/black on green][green] 成功设置坐标"+msg[4:])
+            elif msg[0:5] == "load ":
+                #msg2=msg.split()
+                rprint("[black on green] SUCCESS [/black on green][green] 正在导入"+msg[5:])
+                from rich.progress import track
+                for i in track(range(speed), description='loading...'):
+                    time.sleep(0.2)
+                speed=100
+                rprint("[black on green] SUCCESS [/black on green][green] 成功导入"+msg[5:])
+            else:
+                rprint("[black on yellow] WARNING [/black on yellow][yellow] 未知的命令,尝试输入 menu 获取帮助")
+        #time.sleep(5)
+        rprint("[red] ERROR 与租赁服断开连接[/red]")
+        rprint("[black on yellow] WARNING [/black on yellow][yellow] bythonbuilder将在 5 秒后自动重启")
+        #time.sleep(5)
+        rprint("[black on yellow] WARNING [/black on yellow][yellow] bythonbuilder已退出")
+
+    
